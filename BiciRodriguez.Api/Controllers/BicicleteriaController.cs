4e72618f826e89d1d5e7e4aadb0dd7b92197d7ae
OmarMendezR <omaryesid.dev@gmail.com>
2026-03-29 21:02:17 -0500

    feat: implement BicicleteriaController with async GET for all bicycles

---
+++ BiciRodriguez.Api/Controllers/BicicleteriaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BiciRodriguez.Api.Models; //Referencia de los modelos a usar
+
+namespace BiciRodriguez.Api.Controllers
+{
+    // Atributos de clase
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BicicleteriaController : ControllerBase
+    {
+        private readonly BiciContext _context; // Puente con la DB
+
+        // Constructor (Inyeccion de dependencias)
+        public BicicleteriaController(BiciContext context)
+        {
+            _context = context;
+        }
+        #region METODOS DE LECTURA (GET)
+
+        // Acción para listar todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bicicleta>>> GetBicicletas()
+        {
+            // Consulta Asíncrona
+            return await _context.Bicicletas.ToListAsync();
+        }
+
+        #endregion
+    }
+}
